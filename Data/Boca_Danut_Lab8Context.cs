c13a9d58832e91824eaef0e22b975b96e3d8d2c1
dani boca <bocadanut99@gmail.com>
2020-11-26 15:39:34 +0200

    Add project files.

---
+++ Data/Boca_Danut_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Boca_Danut_Lab8.Models;
+
+namespace Boca_Danut_Lab8.Data
+{
+    public class Boca_Danut_Lab8Context : DbContext
+    {
+        public Boca_Danut_Lab8Context (DbContextOptions<Boca_Danut_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Boca_Danut_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Boca_Danut_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Boca_Danut_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Boca_Danut_Lab8.Data;
+using Boca_Danut_Lab8.Models;
+
+namespace Boca_Danut_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Boca_Danut_Lab8.Data.Boca_Danut_Lab8Context _context;
+
+        public IndexModel(Boca_Danut_Lab8.Data.Boca_Danut_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                 .Include(b => b.Publisher)
+                  .ToListAsync();
+        }
+    }
+}
